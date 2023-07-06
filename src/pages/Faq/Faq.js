46363f2e26f439793ef77d76257e4d00e4a067a9
Suraj Randhari <surajrandhari@gmail.com>
2023-07-06 13:45:14 +0530

    faq1

--- src/pages/Faq/Faq.js
+++ src/pages/Faq/Faq.js
@@ -1,160 +1,158 @@
-import React from 'react';
-// import React, { useState } from 'react';
+// import React from 'react';
+import React, { useState } from 'react';
 import './faq.css'
 
 const Faq = () => {
-  // const initialData = [
-  //   { id: 1, title: 'hellog sadfsd', category: 'sdfoe sjs sga,nms jksdf sdjksdf bjksdf' },
-  //   { id: 2, title: 'sdf sdf dddd', category: 'sdf sdf sdf sd' },
-  //   { id: 3, title: 'hellog', category: 'sdfoe sjs sga,nms jksdf sdjksdf bjksdf sddd ad' },
-  //   {
-  //     id: 4,
-  //     title: 'Non consectetur a erat nam at lectus urna duis?',
-  //     category: 'Feugiat pretium nibh ipsum consequat. Tempus iaculis urna id volutpat lacus laoreet non curabitur gravida. Venenatis lectus magna fringilla urna porttitor rhoncus dolor purus non.',
-  //   },
-  // ];
+  const initialData = [
+    { id: 1, title: 'hellog sadfsd', category: 'sdfoe sjs sga,nms jksdf sdjksdf bjksdf' },
+    { id: 2, title: 'sdf sdf dddd', category: 'sdf sdf sdf sd' },
+    { id: 3, title: 'hellog', category: 'sdfoe sjs sga,nms jksdf sdjksdf bjksdf sddd ad' },
+    {
+      id: 4,
+      title: 'Non consectetur a erat nam at lectus urna duis?',
+      category: 'Feugiat pretium nibh ipsum consequat. Tempus iaculis urna id volutpat lacus laoreet non curabitur gravida. Venenatis lectus magna fringilla urna porttitor rhoncus dolor purus non.',
+    },
+  ];
 
-  // const [data, setData] = useState(initialData);
-  // const [searchQuery, setSearchQuery] = useState('');
+  const [data, setData] = useState(initialData);
+  const [searchQuery, setSearchQuery] = useState('');
 
-  // const handleSearch = (e) => {
-  //   const query = e.target.value;
-  //   setSearchQuery(query);
+  const handleSearch = (e) => {
+    const query = e.target.value;
+    setSearchQuery(query);
 
-  //   const filteredData = initialData.filter(
-  //     (item) =>
-  //       item.title.toLowerCase().includes(query.toLowerCase()) ||
-  //       item.category.toLowerCase().includes(query.toLowerCase())
-  //   );
+    const filteredData = initialData.filter(
+      (item) =>
+        item.title.toLowerCase().includes(query.toLowerCase()) ||
+        item.category.toLowerCase().includes(query.toLowerCase())
+    );
 
-  //   setData(filteredData);
+    setData(filteredData);
+  };
+
+  // const handleSort = (key) => {
+  //   // sorting logic here
   // };
 
-  // // const handleSort = (key) => {
-  // //   // sorting logic here
-  // // };
-
-  // const handleDelete = (id) => {
-  //   const updatedData = data.filter((item) => item.id !== id);
-  //   setData(updatedData);
-  // };
+  const handleDelete = (id) => {
+    const updatedData = data.filter((item) => item.id !== id);
+    setData(updatedData);
+  };
 
   // Sorting logic here
 
   return (
-    // <div>
-    //    <h5 className="card-title">Faq</h5>
-    //         <div className="col-12 d-flex no-block align-items-center">
-    //           <nav aria-label="breadcrumb">
-    //             <ol className="breadcrumb">
-    //                <li className="breadcrumb-item">
-    //                 <a href="/">
-    //                   Home
-    //                 </a>
-    //                 </li>
-    //                 <li className="breadcrumb-item active" aria-current="page">
-    //                   Faq                    
-    //                 </li>
-    //              </ol>
-    //           </nav>
-    //         </div>
-
-    //   <div className="container-fluid">
-    //     <div className="row">
-    //       <div className="col-md-12">
-    //         <div className="card">
-    //           <div className="card-body">
-    //             <h5 className="card-title">Basic Datatable</h5>
-    //             <div className="table-responsive">
-    //               {/* Search bar */}
-    //               <div className="row" style={{ marginBottom: '10px' }}>
-    //                 <div className="col-md-6 d-flex align-items-center">
-    //                   <div className="dataTables_length" id="zero_config_length">
-    //                     <label className="d-flex align-items-center">
-    //                       Show{' '}
-    //                       <select
-    //                         name="zero_config_length"
-    //                         aria-controls="zero_config"
-    //                         className="form-control form-control-sm"
-    //                       >
-    //                         <option value="10">10</option>
-    //                         <option value="25">25</option>
-    //                         <option value="50">50</option>
-    //                         <option value="100">100</option>
-    //                       </select>{' '}
-    //                       entries
-    //                     </label>
-    //                   </div>
-    //                 </div>
-    //                 <div className="col-md-6 d-flex justify-content-end align-items-center">
-    //                   <div id="zero_config_filter" className="dataTables_filter">
-    //                     <label className="d-flex align-items-center">
-    //                       Search:
-    //                       <input
-    //                         type="search"
-    //                         className="form-control form-control-sm ml-2"
-    //                         placeholder=""
-    //                         aria-controls="zero_config"
-    //                         value={searchQuery}
-    //                         onChange={handleSearch}
-    //                       />
-    //                     </label>
-    //                   </div>
-    //                 </div>
-    //               </div>
-
-    //               {/* Table */}
-    //               <div className="row">
-    //                 <div className="col-sm-12">
-    //                   <table
-    //                     id="zero_config"
-    //                     className="table table-striped table-bordered dataTable no-footer"
-    //                     role="grid"
-    //                     aria-describedby="zero_config_info"
-    //                   >
-    //                     <thead>
-    //                       <tr role="row">
-    //                         <th>#</th>
-    //                         <th>Title</th>
-    //                         <th>Category</th>
-    //                         <th>Action</th>
-    //                       </tr>
-    //                     </thead>
-    //                     <tbody>
-    //                       {data.map((item) => (
-    //                         <tr key={item.id} role="row" className="odd">
-    //                           <td className="sorting_1">{item.id}</td>
-    //                           <td>{item.title}</td>
-    //                           <td>{item.category}</td>
-    //                           <td>
-    //                             <button type="button" className="btn btn-sm text-white" style={{ backgroundColor: '#27a9e3'}}>
-    //                               Edit
-    //                             </button>
-    //                             <button
-    //                               type="button"
-    //                               className="btn btn-danger btn-sm text-white"
-    //                               onClick={() => handleDelete(item.id)}
-    //                             >
-    //                               Delete
-    //                             </button>
-    //                           </td>
-    //                         </tr>
-    //                       ))}
-    //                     </tbody>
-    //                   </table>
-    //                 </div>
-    //               </div>
-    //             </div>
-    //           </div>
-    //         </div>
-    //       </div>
-    //     </div>
-    //   </div>
-    // </div>
-
     <div>
-      hiii bbvnvmbnmbnm,, xfddfd df
+       <h5 className="card-title">Faq</h5>
+            <div className="col-12 d-flex no-block align-items-center">
+              <nav aria-label="breadcrumb">
+                <ol className="breadcrumb">
+                   <li className="breadcrumb-item">
+                    <a href="/">
+                      Home
+                    </a>
+                    </li>
+                    <li className="breadcrumb-item active" aria-current="page">
+                      Faq                    
+                    </li>
+                 </ol>
+              </nav>
+            </div>
+
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-md-12">
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">Basic Datatable</h5>
+                <div className="table-responsive">
+                  {/* Search bar */}
+                  <div className="row" style={{ marginBottom: '10px' }}>
+                    <div className="col-md-6 d-flex align-items-center">
+                      <div className="dataTables_length" id="zero_config_length">
+                        <label className="d-flex align-items-center">
+                          Show{' '}
+                          <select
+                            name="zero_config_length"
+                            aria-controls="zero_config"
+                            className="form-control form-control-sm"
+                          >
+                            <option value="10">10</option>
+                            <option value="25">25</option>
+                            <option value="50">50</option>
+                            <option value="100">100</option>
+                          </select>{' '}
+                          entries
+                        </label>
+                      </div>
+                    </div>
+                    <div className="col-md-6 d-flex justify-content-end align-items-center">
+                      <div id="zero_config_filter" className="dataTables_filter">
+                        <label className="d-flex align-items-center">
+                          Search:
+                          <input
+                            type="search"
+                            className="form-control form-control-sm ml-2"
+                            placeholder=""
+                            aria-controls="zero_config"
+                            value={searchQuery}
+                            onChange={handleSearch}
+                          />
+                        </label>
+                      </div>
+                    </div>
+                  </div>
+
+                  {/* Table */}
+                  <div className="row">
+                    <div className="col-sm-12">
+                      <table
+                        id="zero_config"
+                        className="table table-striped table-bordered dataTable no-footer"
+                        role="grid"
+                        aria-describedby="zero_config_info"
+                      >
+                        <thead>
+                          <tr role="row">
+                            <th>#</th>
+                            <th>Title</th>
+                            <th>Category</th>
+                            <th>Action</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          {data.map((item) => (
+                            <tr key={item.id} role="row" className="odd">
+                              <td className="sorting_1">{item.id}</td>
+                              <td>{item.title}</td>
+                              <td>{item.category}</td>
+                              <td>
+                                <button type="button" className="btn btn-sm text-white" style={{ backgroundColor: '#27a9e3'}}>
+                                  Edit
+                                </button>
+                                <button
+                                  type="button"
+                                  className="btn btn-danger btn-sm text-white"
+                                  onClick={() => handleDelete(item.id)}
+                                >
+                                  Delete
+                                </button>
+                              </td>
+                            </tr>
+                          ))}
+                        </tbody>
+                      </table>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+
+   
   );
 };
 
